aaaa7cda452eac721257ff428edc821cac665af4
Lorenz Goebel <lorenzgoebel@gmx.de>
2018-10-23 17:08:22 +0200

    Package name.

--- src/test/java/org/oscm/app/statemachine/StateMachineTest.java
+++ src/test/java/org/oscm/app/statemachine/StateMachineTest.java
@@ -6,7 +6,7 @@
  *                                                                              
  *******************************************************************************/
 
-package oscm.app.statemachine;
+package org.oscm.app.statemachine;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
